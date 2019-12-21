6d227456a5f89e85d4d4fe80779a23c769cf7af7
Heeko <jaheeko@gmail.com>
2019-12-21 01:39:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Cat />
+      <Cat />
+      <Cat />
+    </>
+  )
+}
+
+class Cat extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { number: 0, image: " " };
+
+  }
+
+  async componentDidMount() {
+    const req = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await req.json();
+    this.setState({ image: data[0].url });
+  }
+
+  // https://api.thecatapi.com/v1/images/search
+  render() {
+    return (<div >
+
+      <div className="cats">
+        <img src={this.state.image} width="480px" />
+      </div>
+
+
+      <button onClick={() => { this.setState({ number: this.state.number + 1 }) }}>
+        +
+    </button>
+      <button onClick={() => { this.setState({ number: this.state.number - 1 }) }}>
+        -
+    </button>
+      <p>
+        number: {this.state.number}
+      </p>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
