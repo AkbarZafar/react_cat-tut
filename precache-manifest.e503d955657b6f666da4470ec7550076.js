9e81c588e342fa5a263d0b1b9b9d48438a44208c
Heeko <jaheeko@gmail.com>
2020-02-08 18:15:02 -0500

    Updates

---
+++ precache-manifest.e503d955657b6f666da4470ec7550076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5328e20b7ed3c2f3b83bf7800e94a0bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d231f4e49de606fedf74",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ff17602c8b0a3e2c3ba9",
+    "url": "/static/js/2.df8ac757.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.df8ac757.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d231f4e49de606fedf74",
+    "url": "/static/js/main.1a14603b.chunk.js"
+  },
+  {
+    "revision": "07d5a076a43ad1dda95b",
+    "url": "/static/js/runtime-main.e589c675.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
